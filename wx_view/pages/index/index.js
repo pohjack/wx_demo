b8c626e7890d1016623cec91c066c55c5d33b1da
伍舰 <wu.jian7@ztesoft.com>
2017-12-10 01:43:17 +0800

    前端页面

---
+++ wx_view/pages/index/index.js
@@ -0,0 +1,88 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    // 分类数据
+    classifyInfo: [
+      {
+        img: "/images/classify/classify1.png",
+        name: "美食",
+        backgroundColor: "lightgray"
+      }, {
+        img: "/images/classify/classify2.png",
+        name: "生活用品",
+        backgroundColor: "lightblue"
+      }, {
+        img: "/images/classify/classify3.png",
+        name: "饮料",
+        backgroundColor: "lightgreen"
+      }, {
+        img: "/images/classify/classify4.png",
+        name: "面包",
+        backgroundColor: "lightpink"
+      },
+      {
+        img: "/images/classify/classify1.png",
+        name: "美食",
+        backgroundColor: "lightgray"
+      }, {
+        img: "/images/classify/classify2.png",
+        name: "生活用品",
+        backgroundColor: "lightblue"
+      }, {
+        img: "/images/classify/classify3.png",
+        name: "饮料",
+        backgroundColor: "lightgreen"
+      }, {
+        img: "/images/classify/classify4.png",
+        name: "面包",
+        backgroundColor: "lightpink"
+      }
+      
+    ]
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
